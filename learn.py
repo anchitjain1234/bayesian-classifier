b1f41fb20949374193e91106a56888972f332418
Anchit Jain <anchitjain1234@gmail.com>
2015-09-21 17:52:56 +0530

    Added learning from folder

--- learn.py
+++ learn.py
@@ -2,11 +2,25 @@ from db import Db
 from mode import Mode
 from words import list_to_dict
 from words import text_to_list
-
+import os
 class Learn(Mode):
+	def read_from_dir(self,dirname):
+		for dpath,dnames,fnames in os.walk(dirname):
+			fcontents=''
+			for f in fnames:
+				fcontents+=open(os.path.join(dpath, f), 'r').read()
+		return fcontents
+
+	def get_file_count_from_dir(self,dirname):
+		ct=0
+		for dpath,dnames,fnames in os.walk(dirname):
+			for f in fnames:
+				ct+=1
+		return ct
+
 	def validate(self, args):
 		valid_args = False
-		usage = 'Usage: %s learn <doc type> <file> <count>' % args[0]
+		usage = 'Usage: %s learn <doc type> <file> <count>\n    or %s learn <doc type> <folder>' % (args[0],args[0])
 
 		if len(args) == 5:
 			doc_type = args[2]
@@ -22,6 +36,25 @@ class Learn(Mode):
 				count = int(args[4])
 			except:
 				raise ValueError(usage + '\nEnter an integer value for the "count" parameter')			
+
+			self.file_contents = file_contents
+			self.count = count
+			self.doc_type = doc_type
+
+		elif len(args) == 4:
+			doc_type = args[2]
+			
+			file_contents = None
+			try:
+				file_contents = self.read_from_dir(args[3])
+			except Exception as e:
+				raise ValueError(usage + '\nUnable to read specified directory "%s", the error message was: %s' % (args[3], e))
+
+			count = 0
+			try:
+				count = self.get_file_count_from_dir(args[3])
+			except:
+				raise ValueError(usage + '\nUnable to get file count from specified directory "%s" , the error message was: %s' % (args[3], e))			
 
 			self.file_contents = file_contents
 			self.count = count
